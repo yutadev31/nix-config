052092ca351c5d034379ec0e9fcd0272f6771dad
Yuta <yuta256dev@gmail.com>
2026-03-25 17:19:54 +0900

    chore(ci): add mirror.yaml

--- modules/nixos/common/ssh.nix
+++ modules/nixos/common/ssh.nix
@@ -3,7 +3,6 @@
   services.openssh = {
     enable = true;
     settings = {
-      PasswordAuthentication = false;
       KbdInteractiveAuthentication = false;
       PermitRootLogin = "no";
       AllowUsers = [ "yuta" ];
