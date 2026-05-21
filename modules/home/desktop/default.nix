e3e30c2a4d1304477932760df4bb3dad3e3aef69
Yuta <yuta256dev@gmail.com>
2026-05-21 18:52:11 +0900

    update

--- modules/home/desktop/default.nix
+++ modules/home/desktop/default.nix
@@ -28,4 +28,15 @@
     awww
     cliphist
   ];
+
+  services.gammastep = {
+    enable = true;
+    provider = "manual";
+    latitude = 35.6;
+    longitude = 139.6;
+    temperature = {
+      day = 5000;
+      night = 4000;
+    };
+  };
 }
